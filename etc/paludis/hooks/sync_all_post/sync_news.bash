fa296d04d5ac7155694fc763ae1ac8be544599ce
hasufell <hasufell@hasufell.de>
2015-08-14 17:37:19 +0200

    Beware of repository format update

--- etc/paludis/hooks/sync_all_post/sync_news.bash
+++ etc/paludis/hooks/sync_all_post/sync_news.bash
@@ -17,4 +17,9 @@ fi
 
 cd "${PORTDIR}"/metadata/news || die "failed to cd into ${PORTDIR}/metadata/news"
 git clean -fdxq || die "failed to clean git repo!"
-cp -a */* . || die "could not copy news dirs to base news dir!"
+
+if [[ -n "$(find . -mindepth 1 -maxdepth 1 -type d -name "*-*-*")" ]] ; then
+	die "it seems the repository format of proj/gentoo-news has changed! Update your script!"
+else
+	cp -a */* . || die "could not copy news dirs to base news dir!"
+fi
