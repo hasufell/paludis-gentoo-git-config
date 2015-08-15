19dab7c96993bf605dad2e81121a696659c81e0b
hasufell <hasufell@hasufell.de>
2015-08-15 15:58:01 +0200

    Fix bug in sync_dtd, thanks to freke

--- etc/paludis/hooks/sync_all_post/sync_dtd.bash
+++ etc/paludis/hooks/sync_all_post/sync_dtd.bash
@@ -8,7 +8,7 @@ source "/etc/paludis/hooks/set_portdir.bash"
 cd "${PORTDIR}"/metadata || die "could not cd into '${PORTDIR}/metadata'!"
 if [[ -e dtd ]] ; then
 	einfo "dtd dir already exists, updating..."
-	cd glsa || die "could not cd into 'dtd'!"
+	cd dtd || die "could not cd into 'dtd'!"
 	git pull --ff || die "could not pull updates!"
 else
 	einfo "dtd directory does not exist, cloning..."
