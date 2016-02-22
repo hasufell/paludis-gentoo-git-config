bf2ef87afb6611f85f52c73d660e7c86b641994b
Julian Ospald <hasufell@posteo.de>
2016-02-22 20:59:12 +0100

    Fix echo

--- hooks/sync_post/04-sync_gentoo_projects_xml.bash
+++ hooks/sync_post/04-sync_gentoo_projects_xml.bash
@@ -4,7 +4,7 @@ source "${PALUDIS_EBUILD_DIR}/echo_functions.bash"
 source "/etc/paludis/hooks/set_portdir.bash"
 
 if [[ ${TARGET} == gentoo ]] ; then
-	ebegin "Updating herds.xml"
+	ebegin "Updating projects.xml"
 	wget -q -O "${PORTDIR}"/metadata/projects.xml https://api.gentoo.org/metastructure/projects.xml
 	eend $?
 fi
