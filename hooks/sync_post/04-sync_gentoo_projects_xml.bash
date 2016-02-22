32a674c159720f9f987dce240789f8fb019fab48
Julian Ospald <hasufell@posteo.de>
2016-02-22 20:59:21 +0100

    Use wget timestamping

--- hooks/sync_post/04-sync_gentoo_projects_xml.bash
+++ hooks/sync_post/04-sync_gentoo_projects_xml.bash
@@ -5,6 +5,6 @@ source "/etc/paludis/hooks/set_portdir.bash"
 
 if [[ ${TARGET} == gentoo ]] ; then
 	ebegin "Updating projects.xml"
-	wget -q -O "${PORTDIR}"/metadata/projects.xml https://api.gentoo.org/metastructure/projects.xml
+	wget -q -P "${PORTDIR}"/metadata/ -N https://api.gentoo.org/metastructure/projects.xml
 	eend $?
 fi
